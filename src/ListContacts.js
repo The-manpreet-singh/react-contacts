6b5c3fa82aa21169fdc5027303a56c144a61d3bc
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-12 14:37:50 +0530

    adding propstypes

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 function ListContacts (props)  {
 
@@ -25,6 +25,11 @@ function ListContacts (props)  {
              )}
            </ol>
         )
+    }
+
+    ListContacts.propTypes = {
+        contacts : PropTypes.array.isRequired,
+        deleteHandler: PropTypes.func.isRequired,
     }
 
 export default ListContacts;
