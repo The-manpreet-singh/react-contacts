4c8268a08190691219c7b93e8c497524c278b593
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-12 23:31:15 +0530

    add form

--- src/AddContact.js
+++ src/AddContact.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import ImageInput from './ImageInput'
 
-export default class AddContact extends Component {
+
+class AddContact extends Component {
     render() {
         return (
             <div>
-                Add contact
+                <Link 
+                  className='close-create-contact'
+                  to='/' >
+                  Close
+                </Link>
+                <form className='create-contact-form'>
+                   <ImageInput
+                     className='create-contact-avatar-input'
+                     name='avatarURL'
+                     maxHeight={64} />
+                   <div className='create-contact-details'>
+                       <input type='text' name='name' placeholder='name' />
+                       <input type='text' name='handle' placeholder='Handle' />
+                       <button>Add Contact</button>
+                  </div>  
+                </form>
             </div>
         )
     }
 }
+
+export default AddContact;
