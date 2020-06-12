19c3b5715e69fb380c07e4dbcf531eb5c532f5d0
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-12 15:25:56 +0530

    adding serch bar

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function ListContacts (props)  {
+class ListContacts extends Component {
 
+   static propTypes = {
+    contacts : PropTypes.array.isRequired,
+    deleteHandler: PropTypes.func.isRequired,
+    }
+
+    state={
+        query: '' 
+    }
+
+    updateSearch= (query)=> {
+        this.setState( ()=>({
+            query: query.trim()
+        }) )
+    }
+
+      render(){
         //console.log('Props', this.props);
         return (
-           <ol className='contact-list'>
-             { props.contacts.map( (contact)=> 
+        <div className='list-contacts'>
+            {/* {JSON.stringify(this.state)} */}
+         <div className='list-contacts-top'>
+              <input 
+                 className='search-contacts'
+                 type='text'
+                 placeholder='Search conatacts'
+                 value={this.state.query}
+                 onChange={ (e)=> this.updateSearch(e.target.value) }
+                 />
+         </div>  
+          <ol className='contact-list'>
+             { this.props.contacts.map( (contact)=> 
              <li key={contact.id} className='contact-list-item'>
                  <div 
                    className='contact-avatar' 
@@ -18,18 +45,17 @@ function ListContacts (props)  {
                     <p> {contact.name} </p>
                     <p> {contact.handle} </p>
                  </div>
-                <button onClick={()=>props.deleteHandler(contact)} className='contact-remove'>
+                <button onClick={()=>this.props.deleteHandler(contact)} className='contact-remove'>
                     Remove
                 </button>
              </li>
              )}
            </ol>
+         </div>
+           
         )
     }
-
-    ListContacts.propTypes = {
-        contacts : PropTypes.array.isRequired,
-        deleteHandler: PropTypes.func.isRequired,
-    }
+}
+    
 
 export default ListContacts;
