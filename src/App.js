b9bb56e2ba46f736789c4741cf51de676e70d00a
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-12 21:05:24 +0530

    contact api fetch from server

--- src/App.js
+++ src/App.js
@@ -2,30 +2,41 @@ import React, { Component } from 'react';
 
 import ListContacts from './ListContacts';
 
+import * as ContactsAPI from './utils/ContactsAPI';
+
 
 class app extends Component {
   
   state= {
     contacts : [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
+      // {
+      //   "id": "karen",
+      //   "name": "Karen Isgrigg",
+      //   "handle": "karen_isgrigg",
+      //   "avatarURL": "http://localhost:5001/karen.jpg"
+      // },
+      // {
+      //   "id": "richard",
+      //   "name": "Richard Kalehoff",
+      //   "handle": "richardkalehoff",
+      //   "avatarURL": "http://localhost:5001/richard.jpg"
+      // },
+      // {
+      //   "id": "tyler",
+      //   "name": "Tyler McGinnis",
+      //   "handle": "tylermcginnis",
+      //   "avatarURL": "http://localhost:5001/tyler.jpg"
+      // }
      ]
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+     .then( (contacts)=> {
+       this.setState( ()=> ({
+         contacts
+       }) )
+     } )
   }
 
   removeHanlder = (contact)=> {
